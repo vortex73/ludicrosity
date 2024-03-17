56dc7e9d4c1763c7b240bbf15767be9e78e94091
vortex73 <nsreekumar6@gmail.com>
2024-03-17 21:17:11 +0530

    read write basic

--- src/marked.zig
+++ src/marked.zig
@@ -15,7 +15,6 @@ pub fn callback(conv: [*c]const md.MD_CHAR, size: md.MD_SIZE, userdata: ?*anyopa
         std.log.err("File write failed {}", .{err});
     }
 }
-
 pub fn stroll(dir: []const u8) !void {
     const allocator = std.heap.page_allocator;
     var src_dir = try fs.cwd().openDir(dir, .{ .iterate = true });
@@ -23,6 +22,7 @@ pub fn stroll(dir: []const u8) !void {
 
     var walker = try src_dir.walk(allocator);
     defer walker.deinit();
+    const read_alloc = std.heap.page_allocator;
 
     while (try walker.next()) |unit| {
         if (unit.kind == .file) {
@@ -31,38 +31,25 @@ pub fn stroll(dir: []const u8) !void {
                 return e;
             };
             defer fd.close();
-            try ludicrous(fd, unit.path);
+            const markdown = try fd.readToEndAlloc(read_alloc, 1024 * 1024);
+            try ludicrous(markdown, unit.path);
         }
     }
 }
 
-pub fn ludicrous(source: fs.File, src_path: []const u8) !void {
-    var buffboi = std.io.bufferedReader(source.reader());
-    var input = buffboi.reader();
-    var buffer: [2048]u8 = undefined;
-    //const new: ?*anyopaque = null;
+pub fn ludicrous(markdown: []const u8, src_path: []const u8) !void {
     const htmlFileName = try std.fmt.allocPrint(std.heap.page_allocator, "{s}html", .{src_path[0 .. src_path.len - 2]});
     defer std.heap.page_allocator.free(htmlFileName);
     var htmlFile = try fs.cwd().createFile(htmlFileName, .{});
     defer htmlFile.close();
-    while (try input.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        const val = md.md_html(line.ptr, @intCast(line.len), callback, &htmlFile, @intCast(0), md.MD_HTML_FLAG_DEBUG);
-        if (val == 0) {
-            continue;
-        } else {
-            //  std.debug.print("woo", .{});
-        }
-    }
+    const val = md.md_html(markdown.ptr, @intCast(markdown.len), callback, &htmlFile, @intCast(0), md.MD_HTML_FLAG_DEBUG);
+    _ = val;
 }
 
 pub fn main() !void {
-    //  var source = try std.fs.cwd().openFile("test.md", .{});
-    //const src_dir = "/home/vorrtt3x/dev/ludicrosity/markdwns";
     var buf: [fs.MAX_PATH_BYTES]u8 = undefined;
     const curr = try std.os.getcwd(&buf);
     const alloc = std.heap.page_allocator;
     const src_dir = try fs.path.join(alloc, &[_][]const u8{ curr, "/markdwns/" });
-    // std.debug.print("{s}", .{src_dir});
     try stroll(src_dir);
-    //    defer source.close();
 }
