5f0c08f2f2817d98a31d473da7f7d939b901b01c
vortex73 <nsreekumar6@gmail.com>
2024-04-15 15:46:14 +0530

    routine commit

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
-# Ludicrosity : The SSG
+ ```
+_    _  _ ___  _ ____ ____ ____ ____ _ ___ _   _ 
+|    |  | |  \ | |    |__/ |  | [__  |  |   \_/  
+|___ |__| |__/ | |___ |  \ |__| ___] |  |    |   
 
-> "The Speed is something dangerous but yet very exciting" ~ very wise person
+A Static Site Generator in Zig
+ ```
+
+# Installation
+
+[>] To ensure seamless builds please download the dev release of the zig compiler
+
+```bash
+zig build -Doptimize=ReleaseSafe
+```
+
+# Project Structure
+Ludicrosity looks for the following source tree:
+
+```
+/
+|-- markdwns
+|   |-- (all your markdown source files)
+|-- templates
+|   |-- template.html
+|-- tags(coming soon)
+```
+
+## Metamatter
+Ludicrosity uses a flavor of metadata inspired from [Pandoc](https://pandoc.org/) that I've designated as Metamatter. Goes something like this:
+
+```md
+% title: Post0
+% author: myname
+% date: 17/09/1991
+% tags: new,post,exciting,fast,supreme
+```
+
+... and you write the rest of the post in good ol' markdown.
+
+## Tempating
+No need to learn a new syntax for templating. No. Just use HTML Comments like so:
+
+```html
+<body>
+.
+.
+<!--title-->
+.
+<!--date-->
+..<!--author-->
+
+<!--BODY-->
+</body>
+```
+
+[>] For metamatter fields remember to employ same naming conventions within template comments. `BODY` is reserved for post content.
--- src/marked.zig
+++ src/marked.zig
@@ -5,6 +5,8 @@ const md = @cImport({
     @cInclude("md4c-html.h");
 });
 
+// Refactor: free the arraylist and hashmaps
+
 const Metamatter = struct {
     metadata: std.StringHashMapUnmanaged([]const u8),
     index: usize,
@@ -53,9 +55,10 @@ pub fn stroll(allocator: std.mem.Allocator, dir: []const u8, tempFile: []const u
             };
             defer fd.close();
             const markdown = try fd.readToEndAlloc(allocator, 1024 * 1024);
+            defer allocator.free(markdown);
             const metamatter = try parseMetadata(allocator, markdown);
 
-            try createHtml(allocator, markdown, unit.path, metamatter, tempFile);
+            try createHtml(allocator, markdown, unit.path, metamatter, tempFile, src_dir);
         }
     }
 }
@@ -64,16 +67,18 @@ pub fn bufwriter(underlying_stream: anytype) io.BufferedWriter(1024 * 64, @TypeO
     return .{ .unbuffered_writer = underlying_stream };
 }
 
-pub fn createHtml(allocator: std.mem.Allocator, markdown: []const u8, src_path: []const u8, metamatter: Metamatter, html: []const u8) !void {
+pub fn createHtml(allocator: std.mem.Allocator, markdown: []const u8, src_path: []const u8, metamatter: Metamatter, html: []const u8, src_dir: fs.Dir) !void {
+    // refactor this
     const htmlFileName = try std.fmt.allocPrint(allocator, "{s}html", .{src_path[0 .. src_path.len - 2]});
+    defer allocator.free(htmlFileName);
     var htmlFile = try fs.cwd().createFile(htmlFileName, .{});
     defer htmlFile.close();
 
     var bufferedwriter = bufwriter(htmlFile.writer());
-    try ludicrous(markdown[metamatter.index + 1 ..], &bufferedwriter, bufferedwriter.writer(), metamatter, html);
+    try ludicrous(allocator, markdown[metamatter.index + 1 ..], &bufferedwriter, bufferedwriter.writer(), metamatter, html, src_dir);
     try bufferedwriter.flush();
 }
-pub fn ludicrous(markdown: []const u8, scribe: anytype, writer: anytype, metamatter: Metamatter, html: []const u8) !void {
+pub fn ludicrous(allocator: std.mem.Allocator, markdown: []const u8, scribe: anytype, writer: anytype, metamatter: Metamatter, html: []const u8, src_dir: fs.Dir) !void {
     const x = struct {
         fn callback(conv: [*c]const md.MD_CHAR, size: md.MD_SIZE, userdata: ?*anyopaque) callconv(.C) void {
             const file: *@TypeOf(writer) = @ptrCast(@alignCast(userdata.?));
@@ -90,9 +95,28 @@ pub fn ludicrous(markdown: []const u8, scribe: anytype, writer: anytype, metamat
                 if (std.mem.eql(u8, pointer[start_index + 4 .. end_index], "tags")) {
                     var list = metamatter.tags;
                     var buff: [256]u8 = undefined;
+                    _ = src_dir;
+                    var dir = fs.cwd().openDir("tags/", .{}) catch |err| {
+                        if (err == fs.OpenSelfExeError.FileNotFound) {
+                            std.debug.print("creating tags dir", .{});
+                            return try fs.cwd().makeDir("tags");
+                        } else {
+                            return error.Unexpected;
+                        }
+                    };
+                    defer dir.close();
                     while (true) {
                         const tag = list.popOrNull() orelse break;
-                        const bufw = try std.fmt.bufPrint(&buff, "<li>{s}</li> ", .{std.mem.trim(u8, tag, " ")});
+                        const bufw = try std.fmt.bufPrint(&buff, "<li><a href=\"tags/{s}\">[{s}]</a></li> ", .{ std.mem.trim(u8, tag, " "), std.mem.trim(u8, tag, " ") });
+                        const tagfile = try std.fmt.allocPrint(allocator, "{s}.html", .{tag});
+                        var htmlFile = dir.openFile(tagfile, .{}) catch |err| {
+                            if (err == fs.OpenSelfExeError.FileNotFound) {
+                                std.debug.print("{}", .{err});
+                            } else {
+                                return error.Unexpected;
+                            }
+                        };
+                        defer htmlFile.close();
                         _ = try scribe.write(bufw);
                     }
                 } else {}
@@ -110,7 +134,7 @@ pub fn ludicrous(markdown: []const u8, scribe: anytype, writer: anytype, metamat
 }
 
 pub fn main() !void {
-    const alloc = std.heap.c_allocator;
+    const alloc = std.heap.raw_c_allocator;
     const template = "./templates/template.html";
     const fd = fs.cwd().openFile(template, .{ .mode = .read_only }) catch |e| {
         std.log.err("{s} Can't be opened for reading", .{template});
